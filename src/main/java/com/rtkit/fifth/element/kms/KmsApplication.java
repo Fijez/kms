852eb5cac29ff51d261abe17b9bed4a5ab37c30b
Fijez <tem.est@mail.ru>
2022-06-29 00:28:27 +0300

    пробуем пофиксить pipline

--- src/main/java/com/rtkit/fifth/element/kms/KmsApplication.java
+++ src/main/java/com/rtkit/fifth/element/kms/KmsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class kmsApplication {
+public class KmsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(kmsApplication.class, args);
+        SpringApplication.run(KmsApplication.class, args);
     }
 }
