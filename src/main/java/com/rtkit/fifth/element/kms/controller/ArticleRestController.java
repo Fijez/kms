e2c922b3e20068bfca977118220543e3b378db9c
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2022-07-04 22:51:17 +0300

    исправление

--- src/main/java/com/rtkit/fifth/element/kms/controller/ArticleRestController.java
+++ src/main/java/com/rtkit/fifth/element/kms/controller/ArticleRestController.java
@@ -1,11 +1,15 @@
 package com.rtkit.fifth.element.kms.controller;
 
 import com.rtkit.fifth.element.kms.controller.util.ArticleSearchRequest;
+import com.rtkit.fifth.element.kms.model.dto.ArticleDto;
 import com.rtkit.fifth.element.kms.model.entity.Article;
 import com.rtkit.fifth.element.kms.service.interfaces.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/article")
@@ -13,6 +17,7 @@ public class ArticleRestController {
 
     private final ArticleService articleService;
 
+    @Autowired
     public ArticleRestController(ArticleService articleService) {
         this.articleService = articleService;
     }
@@ -23,7 +28,7 @@ public class ArticleRestController {
     }
 
     @GetMapping(value = "/get", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Article getArticle(@RequestBody ArticleSearchRequest searchRequest) {
-        return null;
+    public List<ArticleDto> getArticle(@RequestBody ArticleSearchRequest searchRequest) {
+        return articleService.searchArticle(searchRequest);
     }
 }
