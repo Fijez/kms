d743ac061fd2806fe6523a235fcc592a40012109
Evgenii Smetanin <smetanin-evgenii@rambler.ru>
2022-07-23 00:28:56 +0300

    проверка доступа к статьям

--- src/main/java/com/rtkit/fifth/element/kms/service/implementation/ArticleServiceImplementation.java
+++ src/main/java/com/rtkit/fifth/element/kms/service/implementation/ArticleServiceImplementation.java
@@ -12,17 +12,18 @@ import com.rtkit.fifth.element.kms.service.interfaces.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -66,7 +67,7 @@ public class ArticleServiceImplementation implements ArticleService {
     }
 
     @Override
-    public Optional<Article> findById(Long id){
+    public Optional<Article> findById(Long id) {
         return articleRepo.findById(id);
     }
 
@@ -76,7 +77,41 @@ public class ArticleServiceImplementation implements ArticleService {
 
         ArticleSpec articleSpec = new ArticleSpec(creator, title, topic, content, tags);
         Slice<Article> articles = articleRepo.findAll(articleSpec, pageable);
-        return articles.map(articleMapper::modelToDto);
+        return filterByAccess(articles).map(articleMapper::modelToDto);
+    }
+
+    private Slice<Article> filterByAccess(Slice<Article> articles) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            throw new RuntimeException("Unauthorized");
+        }
+
+        var grantedAuthorities = authentication.getAuthorities();
+        Set<String> authorities = new HashSet<>();
+        grantedAuthorities.forEach(a -> authorities.add(a.getAuthority()));
+
+        Set<Article> accessibleArticles = new HashSet<>();
+
+        articles.forEach(article -> article.getGroups().forEach(articleGroup -> {
+            if (authorities.contains(articleGroup.getGroup().getTitle().toUpperCase())) {
+                accessibleArticles.add(article);
+            }
+        }));
+
+        articles.forEach(article -> {
+            if (authorities.contains(article.getNamespace().getTitle().toUpperCase())) {
+                accessibleArticles.add(article);
+            }
+        });
+
+        articles.forEach(article -> article.getUsers().forEach(articleUser -> {
+            if (authorities.contains(articleUser.getUserRole().getAuthority().toUpperCase() + "_" + articleUser.getArticle().getTitle().toUpperCase())) {
+                accessibleArticles.add(article);
+            }
+        }));
+
+        return new SliceImpl<>(new ArrayList<>(accessibleArticles), articles.getPageable(), articles.hasNext());
     }
 
     @Override
@@ -91,10 +126,9 @@ public class ArticleServiceImplementation implements ArticleService {
     }
 
 
-
     @Override
     @Transactional
-    public ArticleDto update ( ArticleUpdateDto articleUpdateDto) {
+    public ArticleDto update(ArticleUpdateDto articleUpdateDto) {
         var article = articleRepo.findById(articleUpdateDto.getId()).orElseThrow(() -> new EntityNotFoundException("entity not found"));
         article.setTitle(articleUpdateDto.getTitle());
         article.setVersionDate(LocalDateTime.now(ZoneId.systemDefault()));
